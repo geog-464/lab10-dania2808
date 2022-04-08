bced1ca55a17fb5e93ead2b50839a565ccef9c0d
dania2808 <dania2808@hotmail.com>
2022-04-08 19:03:39 -0400

    final work commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 let myMap;
 
 // we might as well declare our baselayer(s) here too
-const CartoDB_Positron = L.tileLayer(
-	'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
+const OpenTopoMap = L.tileLayer(
+	'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 
 	{
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+		attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 	}
 )
 
@@ -13,20 +13,60 @@ function initialize(){
     loadMap();
 };
 
-function loadMap(){
-	
-	//now reassign the map variable by actually making it a useful object, this will load your leaflet map
-	myMap = L.map('mapdiv', {
-		center: [45.50, -73.58]
-		,zoom: 3
-		,maxZoom: 18
-		,minZoom: 3
-		,layers: CartoDB_Positron
-	});
+function fetchData(){
+    //load the data
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, 
+            	{style: styleAll, pointToLayer: generateCircles, onEachFeature: addPopups}).addTo(myMap);
+        })
+};
+
+function loadMap(mapid){
+	try {
+		myMap.remove()
+	} catch(e) {
+		console.log(e)
+		console.log("no map to delete")
+	} finally {
+	//put your map loading code in here
+		if (mapid=='mapa'){
+		//now reassign the map variable by actually making it a useful object, this will load your leaflet map
+		myMap = L.map('mapdiv', {
+			center: [45.50, -73.58]
+			,zoom: 3
+			,maxZoom: 18
+			,minZoom: 3
+			,layers: OpenTopoMap
+			});
+		// calling function fetchData with data
+			fetchData(url='https://raw.githubusercontent.com/geog-464/lab10/main/data/Amtrak_Stations.geojson');
+
+		}
+
+		if (mapid=='mapb'){
+		//now reassign the map variable by actually making it a useful object, this will load your leaflet map
+			myMap = L.map('mapdiv', {
+				center: [31.24967, 30.06263]
+				,zoom: 2
+				,maxZoom: 15
+				,minZoom: 1
+				,layers: OpenTopoMap
+			});
+			// calling function fetchData with data
+			fetchData(url='https://raw.githubusercontent.com/geog-464/lab10/main/data/megacities.geojson');
+		}
+	}
+
+
 
 	//add the basemap style(s) to a JS object, to which you could also add other baselayers. This object is loaded as a basemap selector as seen further down
 	let baseLayers = {
-		"CartoDB": CartoDB_Positron
+		"OpenTopoMap": OpenTopoMap
 		//,...
 	};
 
@@ -34,6 +74,61 @@ function loadMap(){
 	let lcontrol = L.control.layers(baseLayers);
 	//add it to the map
 	lcontrol.addTo(myMap);
+
+
+	// calling function fetchData with data
+	fetchData();
+
+	console.log(mapid)
+
 };
 
-window.onload = initialize();
+
+
+function generateCircles(feature, latlng) {
+	return L.circleMarker(latlng);
+}
+
+
+function styleAll(feature, latlng) {
+	
+	// console.log(feature.properties.ZipCode);
+
+	var styles = {dashArray:null, dashOffset:null, lineJoin:null, lineCap:null, stroke:false, color:'#000', opacity:1, weight:1, fillColor:null, fillOpacity:0 };
+
+	if (feature.geometry.type == "Point") {
+		styles.fillColor = '#fff'
+		,styles.fillOpacity = 0.5
+		,styles.stroke=true
+		,styles.radius=9
+	}
+
+	if (typeof feature.properties.ZipCode == 'string') {
+		styles.fillColor = '#00FFFF'
+		,styles.fillOpacity = 0.5
+		,styles.stroke=true
+		,styles.radius=9
+	}
+
+	
+	return styles;
+}
+
+function addPopups(feature, layer){
+
+	layer.bindPopup();
+	//console.log(feature)
+	//console.log(layer),
+	//console.log(layer._radius=80)
+	//console.log(layer.options.fill=false)
+	//console.log(layer.getLatLng())
+}
+
+
+
+
+
+
+
+
+//window.onload = initialize();
